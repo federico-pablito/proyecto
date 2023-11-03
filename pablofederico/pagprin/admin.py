c072215b0cf323c9ec8ba664489b72dec8a1192a
FR33TR1ST <gaspartrix@gmail.com>
2023-11-03 08:40:25 -0300

    Revert "Create"
    This reverts commit 6b3826b10e21f2f540c91b2df81475180717947f.

--- pablofederico/pagprin/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from pablofederico.pagprin.models import maestroequipos
-
-# Register your models here.
-admin.site.register(maestroequipos)
--- pablofederico/pagprin/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PagprinConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pagprin'
