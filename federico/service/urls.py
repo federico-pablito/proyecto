65e0fbfe9a549190412a115799777d07c448ce42
FR33TR1ST <89042651+FR33TR1ST@users.noreply.github.com>
2023-11-24 14:24:24 -0300

    lcdtm

--- federico/service/urls.py
+++ federico/service/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('mainservice', views.service_main, name='serviceprin'),
     path('editarserv/<int:id>/', views.editar_serv, name='editar_serv'),
-    path('create_serv', views.crear_serv, name='create-serv')
+    path('create_serv', views.crear_serv, name='create-serv'),
+    path('internos_pd_view', views.internos_pd_view.as_view(), name='internos_pd_view'),
 ]
